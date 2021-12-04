a7373285e015f3fdbb7f55524e6d7d841d074ff7
cameronknaus <cdknaus@gmail.com>
2021-12-04 18:55:27 -0500

    Implement lazy loading for landing page

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import styles from '../Styles/Pages/LandingPage.module.css';
 import KumataCoin from '../Components/LandingPage/KumataCoin';
 import AboutKumata from '../Components/LandingPage/AboutKumata';
 import KumataProfile from '../Components/LandingPage/ProfileSection/KumataProfile';
-import CanadaImage from '../Components/LandingPage/CanadaImage';
-import OnlineLessons from '../Components/LandingPage/ServicesSection/OnlineLessons/OnlineLessons';
-import ServicesHeader from '../Components/LandingPage/ServicesSection/ServicesHeader';
-import OnlineLessonsInDetail from '../Components/LandingPage/ServicesSection/OnlineLessons/OnlineLessonsInDetail';
+
+// Lower end of the page can be lazily loaded
+const CanadaImage = React.lazy(() => import('../Components/LandingPage/CanadaImage'));
+const OnlineLessons = React.lazy(() => import('../Components/LandingPage/ServicesSection/OnlineLessons/OnlineLessons'));
+const ServicesHeader = React.lazy(() => import('../Components/LandingPage/ServicesSection/ServicesHeader'));
+const OnlineLessonsInDetail = React.lazy(() => import('../Components/LandingPage/ServicesSection/OnlineLessons/OnlineLessonsInDetail'));
+
 
 export default function LandingPage() {
     return (
@@ -19,13 +22,21 @@ export default function LandingPage() {
                 <KumataProfile />
             </section>
             <section id='canada-image-section'>
-                <CanadaImage />
+                <Suspense fallback=''>
+                    <CanadaImage />
+                </Suspense>
             </section>
             <section id='offered-services-section'>
-                <ServicesHeader />
+                <Suspense fallback=''>
+                    <ServicesHeader />
+                </Suspense>
                 <div id='online-lessons-section'>
-                    <OnlineLessons />
-                    <OnlineLessonsInDetail />
+                    <Suspense fallback=''>
+                        <OnlineLessons />
+                    </Suspense>
+                    <Suspense fallback=''>
+                        <OnlineLessonsInDetail />
+                    </Suspense>
                 </div>
             </section>
         </>
