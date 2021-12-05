26ef06b85bad971d470c2c1b5639e1413a37d4a7
cameronknaus <cdknaus@gmail.com>
2021-12-04 19:07:56 -0500

    Remove lazy loading for landing page

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,14 +1,18 @@
-import React, { Suspense } from 'react';
+import React  from 'react';
 import styles from '../Styles/Pages/LandingPage.module.css';
 import KumataCoin from '../Components/LandingPage/KumataCoin';
 import AboutKumata from '../Components/LandingPage/AboutKumata';
 import KumataProfile from '../Components/LandingPage/ProfileSection/KumataProfile';
+import CanadaImage from '../Components/LandingPage/CanadaImage';
+import OnlineLessons from '../Components/LandingPage/ServicesSection/OnlineLessons/OnlineLessons';
+import ServicesHeader from '../Components/LandingPage/ServicesSection/ServicesHeader';
+import OnlineLessonsInDetail from '../Components/LandingPage/ServicesSection/OnlineLessons/OnlineLessonsInDetail';
 
 // Lower end of the page can be lazily loaded
-const CanadaImage = React.lazy(() => import('../Components/LandingPage/CanadaImage'));
-const OnlineLessons = React.lazy(() => import('../Components/LandingPage/ServicesSection/OnlineLessons/OnlineLessons'));
-const ServicesHeader = React.lazy(() => import('../Components/LandingPage/ServicesSection/ServicesHeader'));
-const OnlineLessonsInDetail = React.lazy(() => import('../Components/LandingPage/ServicesSection/OnlineLessons/OnlineLessonsInDetail'));
+// const CanadaImage = React.lazy(() => import('../Components/LandingPage/CanadaImage'));
+// const OnlineLessons = React.lazy(() => import('../Components/LandingPage/ServicesSection/OnlineLessons/OnlineLessons'));
+// const ServicesHeader = React.lazy(() => import('../Components/LandingPage/ServicesSection/ServicesHeader'));
+// const OnlineLessonsInDetail = React.lazy(() => import('../Components/LandingPage/ServicesSection/OnlineLessons/OnlineLessonsInDetail'));
 
 
 export default function LandingPage() {
@@ -22,21 +26,13 @@ export default function LandingPage() {
                 <KumataProfile />
             </section>
             <section id='canada-image-section'>
-                <Suspense fallback=''>
-                    <CanadaImage />
-                </Suspense>
+                <CanadaImage />
             </section>
             <section id='offered-services-section'>
-                <Suspense fallback=''>
-                    <ServicesHeader />
-                </Suspense>
+                <ServicesHeader />
                 <div id='online-lessons-section'>
-                    <Suspense fallback=''>
-                        <OnlineLessons />
-                    </Suspense>
-                    <Suspense fallback=''>
-                        <OnlineLessonsInDetail />
-                    </Suspense>
+                    <OnlineLessons />
+                    <OnlineLessonsInDetail />
                 </div>
             </section>
         </>
