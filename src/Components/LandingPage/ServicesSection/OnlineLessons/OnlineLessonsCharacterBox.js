0707c11bd8555e6d80810662a9e89181eb7cae49
cameronknaus <cdknaus@gmail.com>
2021-12-08 12:48:08 -0500

    Add animation for sad to happy kumata

--- src/Components/LandingPage/ServicesSection/OnlineLessons/OnlineLessonsCharacterBox.js
+++ src/Components/LandingPage/ServicesSection/OnlineLessons/OnlineLessonsCharacterBox.js
@@ -1,38 +1,55 @@
 import React from 'react';
 import styles from '../../../../Styles/Components/LandingPage/ServicesSection/OnlineLessons/OnlineLessonsCharacterBox.module.css';
-import regularImage from '../../../../Images/Characters/BashfulKumata.png';
-import webpImage from '../../../../Images/webpDist/BashfulKumata.webp';
+import regularSadKumata from '../../../../Images/Characters/sadKumata.png';
+import webpSadKumata from '../../../../Images/webpDist/sadKumata.webp';
+import regularHappyKumata from '../../../../Images/Characters/happyKumata.png';
+import webpHappyKumata from '../../../../Images/webpDist/happyKumata.webp';
 import Image from '../../../UIElements/Image';
 import VisibilitySensor from 'react-visibility-sensor';
-import { useSprings, animated } from 'react-spring';
+import { useSprings, animated, useTransition, config } from 'react-spring';
 
 export default function OnlineLessonsCharacterBox({ text }) {
     const [doubtsInView, setDoubtsInView] = React.useState(false);
+    const [showHappyKumata, setShowHappyKumata] = React.useState(false);
 
     const handleVisibilityChange = (setToShow) => {
         // Set visibility true only once so the animation doesn't repeat
         setToShow && !doubtsInView && setDoubtsInView(true);
     };
 
-    // Set up animation properties
+    // Set up animation properties for list of doubts
     const doubtsList = text.doubtsList;
     const springsList = useSprings(
         doubtsList.length,
         doubtsList.map((doubtItem, index) => {
-            return {
+            const springGroup = {
                 config: { mass: 5, tension: 1000, friction: 600 },
-                delay: 400 + (index * 800),
+                delay: 400 + (index * 700),
                 to: {
                     opacity: doubtsInView ? 1 : 0,
                     transform: doubtsInView ? 'scale(1)' : 'scale(0.7)'
                 },
                 from: { opacity: 0, transform: 'scale(0.7)' }
             };
+
+            // Begin Kumata transition after the first spring rests
+            if(index === 0) {
+                springGroup.onRest = () => setShowHappyKumata(true);
+            }
+
+            return springGroup;
         })
     );
 
-    const thoughtTextClass = 'small-header-text';
+    // Set up transition between sad and happy kumata
+    const transitions = useTransition(showHappyKumata, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+        config: config.gentle
+    });
 
+    const thoughtTextClass = 'small-header-text';
     return (
         <div className={styles.container}>
             <div className={styles.textWrap}>
@@ -50,10 +67,33 @@ export default function OnlineLessonsCharacterBox({ text }) {
                     </div>
                 </VisibilitySensor>
                 <div className={styles.imageContainer}>
-                    <Image regularImage={regularImage}
-                           webpImage={webpImage}
-                           imageBaseType='png' classes={styles.characterImage}
-                    />
+                    {
+                        transitions(({ opacity }, item) =>
+                            item ? (
+                                <animated.div style={{
+                                        position: 'absolute',
+                                        opacity: opacity.to({ range: [0.0, 1.0], output: [0, 1] })
+                                    }}
+                                >
+                                    <Image regularImage={regularHappyKumata}
+                                           webpImage={webpHappyKumata}
+                                           imageBaseType='png' classes={styles.characterImage}
+                                    />
+                                </animated.div>
+                            ) : (
+                                <animated.div style={{
+                                        position: 'absolute',
+                                        opacity: opacity.to({ range: [1.0, 0.0], output: [1, 0] })
+                                    }}
+                                >
+                                    <Image regularImage={regularSadKumata}
+                                           webpImage={webpSadKumata}
+                                           imageBaseType='png' classes={styles.characterImage}
+                                    />
+                                </animated.div>
+                            )
+                        )
+                    }
                 </div>
                 <div className={`${styles.solutionText} ${thoughtTextClass}`}>
                     {text.mySolution}
