c5e67bbb61118d47845b9dbfaef50d66910abd0f
cameronknaus <cdknaus@gmail.com>
2021-12-20 15:39:39 -0600

    Add Kumata English logo to top of landing page

--- src/Components/LandingPage/KumataCoin.js
+++ src/Components/LandingPage/KumataCoin.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { ReactComponent as Kumata } from '../../Images/SVG/Characters/KumataCoin.svg';
 import styles from '../../Styles/Components/LandingPage/KumataCoin.module.css';
+import Image from '../UIElements/Image';
+import webpHeaderImage from '../../Images/webpDist/KumataEnglish.webp';
+import regularHeaderImage from '../../Images/Other/KumataEnglish.png';
 
 export default function KumataCoin() {
 
     return (
         <div className='average-container'>
+            <div className={styles.headerImageContainer}>
+                <Image webpImage={webpHeaderImage}
+                       regularImage={regularHeaderImage}
+                       classes={styles.headerImage}
+                       imageBaseType='png'
+                       optionalAlt='Kumata English'
+                />
+            </div>
             <Kumata className={styles.svgImageWrap} />
         </div>
     );
